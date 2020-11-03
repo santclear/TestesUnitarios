f7b3d9e3b035f734da8f6075ddfd60e5250766c9
Sant'Clear <santclear@gmail.com>
2020-11-02 22:25:46 -0300

    32. Lançando Exceções

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,15 +1,16 @@
 package br.ce.wcaquino.servicos;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class CalculadoraMockTest {
 
 	@Test
-	public void teste(){
+	public void teste() {
 		Calculadora calc = Mockito.mock(Calculadora.class);
 		Mockito.when(calc.somar(Mockito.eq(1), Mockito.anyInt())).thenReturn(5);
 		
-		System.out.println(calc.somar(1, 100000));
+		Assert.assertEquals(5, calc.somar(1, 100000));
 	}
 }
